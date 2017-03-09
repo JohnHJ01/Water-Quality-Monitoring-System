a80a529d5dd5bf15879aee49c663bed7de0f1daf
JohnHJ01 <chanhj01@hotmail.com>
2017-03-09 17:26:17 +0800

    Update username_password.php

--- Website/username_password.php
+++ Website/username_password.php
@@ -1,8 +1,8 @@
 <?php
 //An external file for storing the credentials for accessing database
-$username = "b9166a6a0b1c0f";
-$password = "a4781a3a";
-$database = "acsm_1d2ae067ee632df";
-$server = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$username = ""; 
+$password = "";
+$database = "";
+$server = ""; //Insert your credentials here
 /*WARNING: MAKE SURE NO EMPTY LINE IS AROUND AFTER ?> SIGN*/
 ?>
